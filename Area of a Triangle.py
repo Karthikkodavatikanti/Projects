7c25f0603ec5e5b4248533ef750d67c720742a68
Karthikkodavatikanti <karthikkodavatikanti@gmail.com>
2024-08-12 14:10:57 +0530

    Create Area of a Triangle.py

---
+++ Area of a Triangle.py
@@ -0,0 +1,7 @@
+# WRITE A PROGRAM TO FIND AREA OF TRIANGLE
+# FORMULA FOR AREA= 0.5*base*height
+
+base=float(input("Enter Base of the Triangel:"))#INPUT
+height=float(input("Enter Height of the Triangle:"))#INPUT
+area=0.5*base*height#PROCESS
+print("Area of Triangle is",area)#OUTPUT
